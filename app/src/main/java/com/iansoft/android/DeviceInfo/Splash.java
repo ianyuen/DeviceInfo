81b7cad230695e450be95f7f93da73c8efb65c10
ianyuen <thanhianyuen@gmail.com>
2016-08-17 23:37:50 +0700

    + start project

---
+++ app/src/main/java/com/iansoft/android/DeviceInfo/Splash.java
@@ -0,0 +1,13 @@
+package com.iansoft.android.DeviceInfo;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Splash extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
---
+++ scripts/config.py
@@ -0,0 +1,5 @@
+RELEASE = 0
+
+TARGET_API = '24'
+APP_NAME = 'DeviceInfo'
+APP_PACKAGE = 'com.iansoft.android.' + APP_NAME
